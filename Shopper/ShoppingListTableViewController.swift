969f073b27b51e2958a1f6533640e8cb068c6dc4
jlyons014 <jlyons014@gmail.com>
2019-11-26 10:33:57 -0500

    Added method to shoppingListTableView

--- Shopper/ShoppingListTableViewController.swift
+++ Shopper/ShoppingListTableViewController.swift
@@ -68,6 +68,12 @@ class ShoppingListTableViewController: UITableViewController {
            
        }
        
+    func deleteShoppingListItem(item: ShoppingListItem){
+        context.delete(item)
+        do{
+            try context.save()
+        }
+    }
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -162,5 +168,37 @@ class ShoppingListTableViewController: UITableViewController {
                 }
             }
     }
+    
+    
+      // Override to support conditional editing of the table view.
+      override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+          // Return false if you do not want the specified item to be editable.
+          return true
+      }
+      
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ShoppingListItemCell", for: indexPath)
+    }
+
+      /*
+      // Override to support editing the table view.
+      override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+          if editingStyle == .delete {
+              // Delete the row from the data source
+              tableView.deleteRows(at: [indexPath], with: .fade)
+          } else if editingStyle == .insert {
+              // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+          }
+      }
+      */
+
+      /*
+      // Override to support conditional rearranging of the table view.
+      override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+          // Return false if you do not want the item to be re-orderable.
+          return true
+      }
+      */
    
 }
